f255831b6fe84f79e03aac82b1d65f4ac69e563b
bdaniele1 <111710795+bdaniele1@users.noreply.github.com>
2022-12-06 23:13:52 -0300

    Create 10.OrdemDecrescente.java

---
+++ 10.OrdemDecrescente.java
@@ -0,0 +1,16 @@
+class OrdemDecrescente {
+
+    public static void main(String[] args) {
+
+        int i;
+
+        System.out.println("Programa para mostrar numeros de maneira decrescente.");
+
+        for (i = 100; i >= 1; i--) {
+            System.out.println("" + i);
+
+        }
+
+    }
+
+}
